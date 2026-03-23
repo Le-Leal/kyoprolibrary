dcce739ae283b224594eaa548772a48b337853b1
Leal <h.okumura0131@gmail.com>
2026-03-23 12:55:46 +0900

    Add Fibonacci character count program in tempo.cpp

---
+++ tempo.cpp
@@ -0,0 +1,92 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(ll i=0;i<n;i++)
+#define srep(i,l,r) for(ll i=l;i<=r;i++)
+#define irep(i,r,l) for(ll i=r;i>=l;i--)
+using ll = long long;
+using ld = long double;
+const ll mod=998244353;
+#define vout(v) for(auto i :v) cout<<i<<" "; cout<<nl;
+#define INF 9223300000000000000ll
+#define Winf 5e12
+#define nl "\n"
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define vl vector<ll>
+#define pb push_back
+#define vc vector<char>
+#define vb vector<bool>
+#define uniq(x) sort((x).begin(),(x).end());(x).erase(unique((x).begin(),(x).end()),(x).end())
+#define eb emplace_back
+
+
+
+void no(void) { cout<<"No"<<nl;}
+void yes(void) {cout<<"Yes"<<nl;}
+void yn(bool a) {
+    cout<<(a ? "Yes":"No")<<nl;
+}
+
+
+vector<ll> dx={-1,0,1,0,1,1,-1,-1};
+vector<ll> dy={0,-1,0,1,-1,1,-1,1};
+
+bool isin(ll i,ll j,ll h,ll w) {
+    if(i<0 || i>=h || j<0 || j>=w) return false;
+    return true;
+}
+
+template<typename T> bool chmin(T& a, T b){if(a > b){a = b; return true;} return false;}
+template<typename T> bool chmax(T& a, T b){if(a < b){a = b; return true;} return false;}
+
+
+template<class T>T vecmax(const vector<T>&v){return *max_element(all(v));}
+template<class T>T vecmin(const vector<T>&v){return *min_element(all(v));}
+
+ll safemod(ll num,ll rule) {
+    return (num%rule+rule)%rule;
+}
+
+ll sum(vector<ll> &a) {
+    return accumulate(all(a),0ll);
+}
+template<class T>void vvpr(vector<vector<T>> g) {
+    rep(i,g.size()) {
+        cout<<"here: i="<<i<<" ";
+        rep(j,g[i].size()) {
+            cout<<g[i][j]<<(j==g[i].size()-1 ? "\n":" ");
+        }
+    }
+}
+
+
+int main() {
+    string x,y; cin>>x>>y;
+    vector<ll> fib(100);
+    fib[0]=x.size(),fib[1]=y.size();
+    ll limit;
+    srep(i,2,99) {
+        fib[i]=fib[i-1]+fib[i-2];
+        if(fib[i]>=1000000000000000000ll) limit=i;
+    }
+    vector<vl> cnt(lim+1,vl(26));
+    for(char c:x) cnt[0][c-'a']++;
+    for(char c:y) cnt[1][c-'a']++;
+    srep(i,2,lim) {
+        rep(j,26) {
+            cnt[i][j]=cnt[i-1][j]+cnt[i-2][j];
+        }
+    }
+    char c;
+    function<ll(ll,ll)> recurse=[&](ll n,ll lim)->ll {
+        if(n==0 || n==1) return cnt[n][c-'a'];
+    };
+    ll q; cin>>q;
+    rep(i,q) {
+        ll l,r;
+        cin>>l>>r>>c;
+        cout<<recuse(r)-recurse(l-1);
+    }
+}
